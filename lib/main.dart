9e5e35cf1d53ed0615181e97ec490dea3c424380
joaobisi <jbisinotti@gmail.com>
2023-04-29 22:41:13 -0300

    modificacao da main para rodar o jogo
    Co-authored-by: pedrohelias <pedrohelias95@hotmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:tictactoe/pages/jogo.dart';
+import 'core/constants.dart';
+import 'core/tema.dart';
 
 void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: nomeJogo,
+      theme: tema,
+      home: const Jogo(),
+    );
+  }
 }
